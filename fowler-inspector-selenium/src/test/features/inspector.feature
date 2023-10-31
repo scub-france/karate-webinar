db61269d88238bf9fb1ecdf609fec3ad52f1eb25
pjmalandrino <pierjean.malandrino@scub.net>
2023-10-31 10:39:40 +0100

    Init fowler inspector in Salenium

--- fowler-inspector-selenium/src/test/features/inspector.feature
+++ fowler-inspector-selenium/src/test/features/inspector.feature
@@ -3,10 +3,10 @@ Feature: Navigation on Martin Fowler website to verify accessibility of data
   Scenario Outline: Verify Navigation and Elements:
     Given I am on the base URL
     When I click on the link with href "<nav_path>"
-    Then I wait for an element containing text "<nav_text>"
+    And I wait for an element containing text "<nav_text>"
     And I verify an element with href "<existing_path>" exists
 
     Examples:
       # Here you'd list the rows of data from your data.json. It's static in Cucumber unlike Karate.
       | nav_path | nav_text | existing_path |
-      | ...      | ...      | ...           |
+      | /architecture | Software Architecture Guide | /microservices  |
--- fowler-inspector-selenium/src/test/java/glue/StepDefinitions.java
+++ fowler-inspector-selenium/src/test/java/glue/StepDefinitions.java
@@ -13,12 +13,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class StepDefinitions {
     WebDriver driver = new ChromeDriver();
 
     @Given("I am on the base URL")
     public void i_am_on_the_base_URL() {
-        driver.get("YOUR_BASE_URL_HERE");  // replace with your actual base URL
+        driver.get("https://martinfowler.com");
     }
 
     @When("I click on the link with href {string}")
@@ -26,17 +28,15 @@ public class StepDefinitions {
         WebElement link = driver.findElement(By.xpath("//a[@href='" + navPath + "']"));
         link.click();
     }
-
-    @Then("I wait for an element containing text {string}")
-    public void i_wait_for_an_element_containing_text(String navText) {
-        // You'd typically use WebDriverWait for this:
-        new WebDriverWait(driver, 10).until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[contains(text(),'" + navText + "')]"), navText));
+    @And("I wait for an element containing text {string}")
+    public void i_wait_for_an_element_containing_text(String textContent) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // or any other desired timeout duration
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(),'" + textContent + "')]")));
     }
-
     @And("I verify an element with href {string} exists")
     public void i_verify_an_element_with_href_exists(String existingPath) {
         WebElement element = driver.findElement(By.xpath("//a[@href='" + existingPath + "']"));
-        Assert.assertNotNull(element);  // Assuming you're using JUnit assertions
+        Assert.assertNotNull(element);
     }
 
     @After
